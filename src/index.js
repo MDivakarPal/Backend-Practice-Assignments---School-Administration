9b21851f33bed3bc27efb46c0d1a17a3bc329d3a
Divakar Pal <31034949+MDivakarPal@users.noreply.github.com>
2021-06-15 02:29:01 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
-const express = require('express')
-const app = express()
-const bodyParser = require("body-parser");
-const port = 8080
-app.use(express.urlencoded());
-
-// Parse JSON bodies (as sent by API clients)
-app.use(express.json());
-app.use(bodyParser.json())
-// your code goes here
+const express = require("express");
+const app = express();
+const port = 8080;
 const studentArray1 = require("./InitialData");
 let studentArray = [...studentArray1];
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 // your code goes here
 app.get("/", (req, res) => {
-  res.status(200).
-  
-  ("Home ");
+  res.status(200).send("Home ");
 });
 
 //To get data of all students in array of json formats
@@ -30,7 +21,7 @@ app.get("/api/student/:id", (req, res) => {
   const faund = studentArray.find((stu) => stu.id == id);
   console.log(faund);
   if (faund) res.status(200).json(faund);
-  else res.status(404);
+  else res.status(404).send("Failed");
 });
 
 //delete a element from the array
@@ -44,13 +35,13 @@ app.delete("/api/student/:id", (req, res) => {
       isId = true;
     }
   });
-  isId ? res.status(200) : res.status(404);
+  isId ? res.status(200).send("ok") : res.status(404);
 });
 
 //adding a new data in
 app.post("/api/student", (req, res) => {
   const { name, currentClass, division } = req.body;
-  if (!name || !currentClass || !division) res.status(400);
+  if (!name || !currentClass || !division) res.status(400).send("fail");
   studentArray = [
     ...studentArray,
     { name, currentClass, division, id: studentArray1.length + 1 },
@@ -60,12 +51,12 @@ app.post("/api/student", (req, res) => {
 
 //Putting / update request
 app.put("/api/student/:id", (req, res) => {
-  const { name, currentClass, division } = JSON.parse(Object.keys(req.body)[0]);
+  //const { name, currentClass, division } = JSON.parse(Object.keys(req.body)[0]);
   const data=req.body;
   console.log(data)
   const { id } = req.params;
   console.log(id);
-  if (!name && !currentClass && !division) res.status(400);
+  /*if (!name && !currentClass && !division) res.status(400);
 
   let isValid = false;
   studentArray1.forEach((ele, idx) => {
@@ -77,7 +68,7 @@ app.put("/api/student/:id", (req, res) => {
       isValid = true;
     }
   });
-  isValid ? res.status(200).send : res.status(400);
+  isValid ? res.status(200).send("Updated") : res.status(400).send(id);*/
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
